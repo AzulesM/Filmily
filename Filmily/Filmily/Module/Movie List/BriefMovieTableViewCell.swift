bc80e4035e31786c83df9142a372e23d5e85b970
Liwei <lw.mai@maisense.com>
2018-09-16 17:44:03 +0800

    Add Loading image as a placeholder while requesting a web image

--- Filmily/Filmily/Module/Movie List/BriefMovieTableViewCell.swift
+++ Filmily/Filmily/Module/Movie List/BriefMovieTableViewCell.swift
@@ -28,7 +28,7 @@ class BriefMovieTableViewCell: UITableViewCell {
         let urlString = movie.backdrop_path ?? movie.poster_path
         let url = URL(string: baseURL + urlString!)
         
-        movieImageView.sd_setImage(with: url, placeholderImage: nil, options: .retryFailed) { [weak self] (image, error, cacheType, imageURL) in
+        movieImageView.sd_setImage(with: url, placeholderImage: UIImage(named: "Loading"), options: .retryFailed) { [weak self] (image, error, cacheType, imageURL) in
             if error != nil {
                 self?.movieImageView.image = UIImage(named: "Oops")
             }
